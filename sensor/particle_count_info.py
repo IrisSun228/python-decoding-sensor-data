132b833cebfde4f3a2d57cf90fec1d90d9ed2fa0
Iris Sun <irissun@live.com>
2023-01-27 20:21:26 -0500

    Section 4 Step 8 - Convert Particle Count Data

--- sensor/particle_count_info.py
+++ sensor/particle_count_info.py
@@ -5,3 +5,8 @@ from datetime import date
 class ParticleData(HouseInfo):
     def _convert_data(self, data):
         recs = []
+
+        for rec in data:
+            recs.append(float(rec))
+
+        return recs
