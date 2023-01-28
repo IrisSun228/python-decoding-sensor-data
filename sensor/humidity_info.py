4cdfedd684477ee6f1fae2ce902e43c1e4f1f4dc
Iris Sun <irissun@live.com>
2023-01-27 20:12:41 -0500

    Section 4 Step 2 - Convert Humidity Data

--- sensor/humidity_info.py
+++ sensor/humidity_info.py
@@ -5,3 +5,9 @@ from datetime import date
 class HumidityData(HouseInfo):
     def _convert_data(self, data):
         recs = []
+
+        for rec in data:
+            # Convert string of integers into actual integers based 10
+            recs.append(float(rec) * 100)
+
+        return recs
